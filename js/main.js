ebb56185e7048adba5c8aa6fe8e7bf02e8e19c40
Oh JuYeon <dpffpself@naver.com>
2022-09-07 13:28:43 +0900

    2022-09-07

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { Modal_Ctrl } from "./Modal.js";
-import { Resizable } from "./Resizable.js";
+// import { Resizable } from "./Resizable.js";
 
 // const RESIZE = new Resizable();
 const MODAL = new Modal_Ctrl();
